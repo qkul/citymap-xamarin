03c6e31d0935b6e0b3756fe0cdef8ac3112ec603
Andrei Kulbeda <Andrei_Kulbeda@epam.com>
2019-04-24 20:27:28 +0300

    internet connection

--- CityMapXamarin/Services/CityService.cs
+++ CityMapXamarin/Services/CityService.cs
@@ -6,14 +6,21 @@ using System.Threading.Tasks;
 using CityMapXamarin.Infrastructure;
 using CityMapXamarin.Models;
 using Newtonsoft.Json;
+using Plugin.Connectivity;
 
 namespace CityMapXamarin.Services
 {
     public class CityService : ICityService
     {
         private const string ApiUrl = "https://api.myjson.com/bins/7ybe5";
+        private const string NoInternetConnection = "Sorry but no connection ;(";
         public async Task<IEnumerable<City>> LoadCitiesAsync()
         {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                throw new Exception(NoInternetConnection);
+            }
+
             using (var httpClient = new HttpClient())
             {
                 var responseJson = await httpClient.GetStringAsync(ApiUrl);
